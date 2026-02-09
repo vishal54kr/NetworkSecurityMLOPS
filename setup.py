91b9cb3641e35ad6c5462e769e3e4c7e1974fa06
vishal54kr <vishalk190597@gmail.com>
2026-02-09 08:25:42 +0100

    setup.py and logger file created

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    Thiss function will return list of requirements
+    
+    """
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #Read lines from the file
+            lines=file.readlines()
+            ## Process each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirement_lst
+
+setup(
+    name="NetworkSecurityMLOPS",
+    version="0.0.1",
+    author="Vishal Kumar",
+    author_email="vishalk190597@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
